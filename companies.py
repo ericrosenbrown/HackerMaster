f7b81d219a546010a3856afa43447b1338e77a59
willemhelmet <willemhelmet@gmail.com>
2017-11-18 15:26:32 -0500

    added companies.py

---
+++ companies.py
@@ -0,0 +1,3 @@
+firstPart  = ['vista', 'super', 'simply', 'manager', 'safe', 'crazy', 'the best', 'price', 'custom', 'pocket', 'express']
+secondPart = ['engine', 'guru', 'people', 'ology', 'for kids' , 'monkey', 'for all', 'age', 'watch', '3d', 'three thousand', 'two thousand', 'one thousand']
+thirdPart  = ['corporation', 'incorperated', 'worldwide', 'and sons', 'lld', 'ltd', 'inc']
