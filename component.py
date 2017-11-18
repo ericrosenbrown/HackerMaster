9ae6bda13c97bdc20ccc624dea391df47af43414
David Whitney <david_whitney@brown.edu>
2017-11-18 17:31:41 -0500

    added different exiting intents, and audio stuff

--- component.py
+++ component.py
@@ -4,6 +4,8 @@ import passphrase as pp
 from random import randint
 import random
 
+from xml.etree import ElementTree
+
 from flask import Flask, render_template
 
 from flask_ask import Ask, statement, question, session, context, audio, current_stream
@@ -15,6 +17,14 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+def my_output_speech(speech):
+    try:
+        xmldoc = ElementTree.fromstring(speech)
+        if xmldoc.tag == 'speak':
+            return {'type': 'SSML', 'ssml': speech}
+    except (UnicodeEncodeError, ElementTree.ParseError) as e:
+        pass
+    return {'type': 'PlainText', 'text': speech}
 
 @ask.launch
 def new_game():
@@ -24,12 +34,12 @@ def new_game():
     stream_url = 'https://www.vintagecomputermusic.com/mp3/s2t9_Computer_Speech_Demonstration.mp3'
     return_value = audio(welcome_msg).play(stream_url)
     return_value._response['shouldEndSession'] = False
+    reprompt = {'outputSpeech': my_output_speech('')}
+    return_value._response['reprompt'] = reprompt
     return return_value
 
-    #return question(welcome_msg)
 
-
-@ask.intent("YesIntent")
+@ask.intent("AMAZON.YesIntent")
 def next_round():
     if session.attributes['in_game'] == False:
         session.attributes['lives'] = 3
@@ -46,6 +56,9 @@ def next_round():
         return question(msg)
     if session.attributes['in_game'] == True:
         return checkpass('dummy','dummy','dummy')
+
+
+
 
 @ask.intent("AnswerIntent", convert={'stepone': str, 'steptwo': str, 'stepthree': str})
 def checkpass(stepone,steptwo,stepthree):
@@ -89,18 +102,13 @@ def checkpass(stepone,steptwo,stepthree):
     return question(msg)
 
 
+@ask.intent('AMAZON.NoIntent')
 @ask.intent('AMAZON.PauseIntent')
-def pause():
-    return_value = audio('').stop()
-    return_value._response['shouldEndSession'] = True
-    return return_value
-
-
 @ask.intent('AMAZON.StopIntent')
-def stop():
-    return_value = audio('').stop()
-    return_value._response['shouldEndSession'] = True
-    return return_value
+@ask.intent('AMAZON.CancelIntent')
+def stop_audio_and_exit():
+    return audio('').stop()
+
 
 
 if __name__ == '__main__':
